ffd897aae3c383d6c8a537e8bf4a0b00ce55fc20
Adam Crow <adamcrow63@gmail.com>
2020-10-27 21:55:30 +1100

    Updated docekr building

--- push.sh
+++ push.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-#function prop() {
-#  grep "${1}=" ${file} | cut -d'=' -f2
-#}
-version=7.2.0
-#version=$(prop 'git.build.version')
+version=7.3.0
 project=${PWD##*/}
 file="src/main/resources/${project}-git.properties"
 
@@ -15,17 +11,9 @@ else
 fi
 
 if [ -f "$file" ]; then
-  echo "$file found."
-  echo "git.commit.id = " "$(prop 'git.commit.id')"
-  echo "git.build.version = " "$(prop 'git.build.version')"
-
   docker push gennyproject/${project}:"${version}"
-
   docker tag gennyproject/${project}:"${version}" gennyproject/${project}:latest
   docker push gennyproject/${project}:latest
-
-  docker tag gennyproject/${project}:"${version}" gennyproject/${project}:"$(prop 'git.build.version')"
-  docker push gennyproject/${project}:"$(prop 'git.build.version')"
 else
   echo "ERROR: git properties $file not found."
 fi
