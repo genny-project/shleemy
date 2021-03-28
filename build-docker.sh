9f6b9b266aa434d9fef5fbed7fe4c0baecbd6c29
Adam Crow <adamcrow63@gmail.com>
2021-03-29 09:26:04 +1100

    Updated build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-version=$(grep 'git.build.version')
-pwd=${PWD##*/}
-function prop() {
-  grep "${1}=" ${file} | cut -d'=' -f2
-}
+project=${PWD##*/}
+realm=gennyproject
+version=$(cat src/main/resources/${project}-git.properties | grep 'git.build.version' | cut -d'=' -f2)
+echo "project ${project}"
+echo "version ${version}"
+branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+commit_hash=$(git rev-parse HEAD)
 
+if [ -z "${1}" ]; then
+  version=$(cat src/main/resources/${project}-git.properties | grep 'git.build.version' | cut -d'=' -f2)
+else
+  version="${1}"
+fi
 
-echo "Building docker gennyproject/${pwd}:${version}"
-./mvnw clean package -Dquarkus.container-image.build=true -DskipTests=true
-docker tag ${USER}/${pwd}:${version}  gennyproject/${pwd}:latest 
-docker tag ${USER}/${pwd}:${version}  gennyproject/${pwd}:${version} 
+# build docker imges
+echo "Git branch:" "${branch}"
+echo "Last git commit hash:" "${commit_hash}"
+docker  build --no-cache -f src/main/docker/Dockerfile.jvm  -t  ${realm}/${project}:latest .
+
+#clean up
+image_ids=$(docker images | grep ${project} | grep none)
+if [ "${image_ids:-0}" == 0 ]; then
+  echo 'Skip clean up'
+else
+  docker images | grep ${project} | grep none | awk '{print $3}' | xargs docker rmi
+fi
+
+docker tag ${realm}/${project}:latest ${realm}/${project}:${version}
--- build.sh
+++ build.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 ./mvnw clean package -DskipTests=true
-#./mvnw clean package -DskipTests=false
-docker stop mysql-1
-docker rm -f mysql-1
